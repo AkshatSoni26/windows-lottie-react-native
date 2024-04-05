e14cbec6d9197d7917387748ebd0c8bd7b8d5851
AkshatSoni26 <135410361+AkshatSoni26@users.noreply.github.com>
2024-04-05 18:45:36 +0530

    .

---
+++ react-native.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    project: {
+      windows: {
+        sourceDir: 'windows',
+        solutionFile: 'sampleDesktop.sln',
+        project: {
+          projectFile: 'sampleDesktop\\sampleDesktop.vcxproj',
+        },
+      },
+    },
+  };
